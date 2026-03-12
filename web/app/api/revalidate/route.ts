13f2fbe92f84e7ba82740cb51d24f725c43cd3d8
ahmedghazi <atmet.ghazi@gmail.com>
2026-03-12 16:07:20 +0100

    REVALIDATE_SECRET

--- web/app/api/revalidate/route.ts
+++ web/app/api/revalidate/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get("secret");
+  console.log("REVALIDATE_CACHE");
   console.log("secret", secret);
   //
   // Vérifie le token secret
